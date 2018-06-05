f6141bf27704e32db30765d6822d1b3193a0f96e
Bhagavathi Chirra <bhagavathi.chirra@gmail.com>
2018-06-05 19:03:21 +1000

    added flights route

---
+++ burning-server/app/controllers/flights_controller.rb
@@ -0,0 +1,23 @@
+class FlightsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @flights = Flight.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
