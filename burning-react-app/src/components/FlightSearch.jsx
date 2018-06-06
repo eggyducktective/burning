d1a1000fc3ac3f7cf5289af94027dc45cbed6a4d
Brendan Leonard <brendanleonard99@gmail.com>
2018-06-06 15:13:33 +1000

    First working version of flight search using react

--- burning-react-app/src/components/FlightSearch.jsx
+++ burning-react-app/src/components/FlightSearch.jsx
@@ -1,51 +1,138 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import FlightSearchForm from '../components/FlightSearchForm.jsx'
-
-// Needed for axios
 const SERVER_URL = 'http://localhost:3000/search.json';
 
-class FlightSearch extends Component {
+class FlightSearchForm extends Component {
 
-  constructor(){
-    super();
+  constructor( props ){
+    super( props );
 
     this.state = {
-      flights: []
+      flights: [],
+      originList: [],
+      destinationList: [],
+      origin: '',
+      destination: '',
+      searchResults: []
     }
+
+    this._handleOriginChange = this._handleOriginChange.bind( this );
+    this._handleDestinationChange = this._handleDestinationChange.bind( this );
+    this._handleSubmit = this._handleSubmit.bind( this );
   }
 
-  componentWillMount(){
-    // Runs before the first render
+  setOriginList(){
+    for( let key in this.state.flights ){
+      const origin = this.state.flights[ key ].origin;
+      // If origin is new add it to the origins array in state
+      if( !this.state.originList.includes ( origin ) ){
+        this.setState(prevState => ({
+          originList: [...prevState.originList, origin]
+        }));
+      }
+    }
+    // Set initial origin in case form is submitted before the user makes a change
+    this.setState({ origin: this.state.originList[0] });
   }
 
+  setDestinationList(){
+    for( let key in this.state.flights ){
+      const destination = this.state.flights[ key ].destination;
+      // If destination is new add it to the destinations array in state
+      if( !this.state.destinationList.includes ( destination ) ){
+        this.setState(prevState => ({
+          destinationList: [...prevState.destinationList, destination]
+        }));
+      }
+    }
+    // Set initial destination in case form is submitted before the user makes a change
+    this.setState({ destination: this.state.destinationList[0] });
+  }
+
+  // Needed for axios
   componentDidMount(){    
     // Put it in here - this will run AFTER the component mounts and has done a render() once    
     const fetchFlights = () => axios.get( SERVER_URL )
     .then( response => {
       this.setState({ flights: response.data });
+      this.setOriginList();
+      this.setDestinationList();
     });
 
     fetchFlights();
   }
 
+  _handleSubmit( event ){
+    event.preventDefault();
+    const originToFind = this.state.origin;
+    const destinationToFind = this.state.destination;
+    const searchResults2 = [];
+
+    // Run through all flights and construct matching list
+    for (let i = 0; i < this.state.flights.length; i++) {
+      const flight = this.state.flights[i];
+      if ( flight.origin === originToFind && flight.destination === destinationToFind ){
+        searchResults2.push( flight );
+      }
+    }
+    console.log('a1', searchResults2);
+
+    this.setState({ searchResults: searchResults2 });
+  }
+
+  _handleOriginChange( event ){
+    this.setState({ origin: event.target.value });
+  }
+  
+  _handleDestinationChange( event ){
+    this.setState({ destination: event.target.value });
+  }
+
   render () {
     return (
-      <div className="App">
-        <h1>Flight Search Results</h1>
-        {/* <FlightSearchForm reportSecret={ this.saveSecret }/> */}        
-        <FlightSearchForm flights={ this.state.flights } />
-          <ul>
+      <div>
+      <form onSubmit={ this._handleSubmit }>
+        <label htmlFor="origin">Origin: </label>
+        <select onChange={ this._handleOriginChange } >
           {
-            this.state.flights.length && this.state.flights.map(
-              f => <li key={ f.id } >{ f.flight_number } { f.flight_date } { f.origin } { f.destination } { f.plane } </li>
-            )
+          this.state.originList.length && this.state.originList.map( f => <option key={ f }>{ f }</option> )
           }
-          </ul>
-    </div>
+        </select>
+        &nbsp;&nbsp;
+        <label htmlFor="destination">Destination: </label>
+        <select onChange={ this._handleDestinationChange } >
+          {
+          this.state.destinationList.length && this.state.destinationList.map( f => <option key={ f }>{ f }</option> )
+          }
+        </select>
+        &nbsp;&nbsp;
+        <input type="submit" value="Search for Flights"/>
+        </form> 
+
+        <br />
+        <br />
+          <table className="searchResults">
+            <tr>
+              <td>Flight Number</td>
+              <td>Date</td>
+              <td>Origin</td>
+              <td>Destination</td>
+            </tr>
+        {
+        this.state.searchResults.length && this.state.searchResults.map((flight, index) => (
+        <tr key={ index }>
+          <td>{ flight.flight_number }</td>
+          <td>{ flight.flight_date }</td>
+          <td>{ flight.origin }</td>
+          <td>{ flight.destination }</td>
+        </tr>
+        
+        ))}
+        </table>
+      </div>  
     );
   }
 }
 
-export default FlightSearch; 
+export default FlightSearchForm;
