f6a5aee67630f9be2a142d12c6dd76589ff5aa7a
Brendan Leonard <brendanleonard99@gmail.com>
2018-06-06 22:37:56 +1000

    Changes to support reservation react components

---
+++ burning-react-app/src/components/Reservation.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import FlightDetails from '../components/FlightDetails.jsx' 
+import SeatMap from '../components/SeatMap.jsx' 
+
+const SERVER_URL_PREFIX = 'http://localhost:3000/flights';
+
+class Reservation extends Component {
+
+  constructor( props ){
+    super( props );
+
+    this.state = {
+      flight: []
+    }
+  }
+
+  // Needed for axios
+  componentDidMount(){
+    // Put it in here - this will run AFTER the component mounts and has done a render() once
+    const SERVER_URL = `${ SERVER_URL_PREFIX }/${ this.props.match.params.id }.json`;
+
+    const getSingleFlightDetail = () => axios.get( SERVER_URL )
+    .then( response => {
+      this.setState({ flight: response.data });
+    });
+
+    getSingleFlightDetail();
+  }
+  
+  render () {
+    return (
+      <div>
+        <h1>This is in Reservation</h1>
+        <FlightDetails 
+          flight={ this.state.flight }
+        />
+        <SeatMap />
+      </div>
+    )
+  }
+}
+
+export default Reservation;
