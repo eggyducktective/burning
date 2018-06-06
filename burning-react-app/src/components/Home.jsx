f4f7a82d757d309f636452d99bc8306def2dcc1c
Brendan Leonard <brendanleonard99@gmail.com>
2018-06-06 22:42:08 +1000

    Changes to add new reservation link to Home.jsx

--- burning-react-app/src/components/Home.jsx
+++ burning-react-app/src/components/Home.jsx
@@ -8,14 +8,8 @@ class Home extends Component {
     return(
       <div>
         <h1>Welcome to Burning Airlines Service</h1>
-<<<<<<< HEAD
           <li>Why not book a flight right now? You deserve it. <Link to="/flight"> Search for Flight</Link></li>
           <li>Found your flight? Want to make a booking? <Link to="/reservation"> Book a Flight</Link></li>
-=======
-          <li>Why not book a flight right now? You deserve it. <Link to="/flight"> Flight Search</Link></li>
-
-
->>>>>>> 58d1120016cbea7846240a9ff71977af4aa4f0af
       </div>
     );
   }
